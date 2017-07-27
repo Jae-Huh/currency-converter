1f09e052537dea29dfa05463d75d300917e3e163
Jae Huh <jaeeunhuh@gmail.com>
2017-07-27 18:38:14 +1000

    Animate logo depending whether the keyboard is open or not - Home Screen: Animating the Logo

--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -5,23 +5,27 @@ import EStyleSheet from 'react-native-extended-stylesheet'
 const imageWidth = Dimensions.get('window').width / 2
 
 export default EStyleSheet.create({
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
   container: {
     alignItems: 'center',
   },
   containerImage: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth
+    width: '$largeContainerSize',
+    height: '$largeContainerSize',
   },
-  image: {
-    width: imageWidth / 2
+  logo: {
+    width: '$largeImageSize',
   },
   text: {
     color: '$white',
     fontWeight: '600',
     fontSize: 28,
     letterSpacing: -0.5,
-    marginTop: 15
+    marginTop: 15,
   },
 })
