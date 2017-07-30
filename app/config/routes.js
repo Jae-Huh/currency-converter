5011c213b5b88896c34c58c0065a2046be966912
Jae Huh <jaeeunhuh@gmail.com>
2017-07-30 20:07:35 +1000

    Navigate to options and themes screen - Options Stack Navigator

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,8 +3,10 @@ import { StackNavigator } from 'react-navigation'
 
 import Home from '../screens/Home'
 import CurrencyList from '../screens/CurrencyList'
+import Options from '../screens/Options'
+import Themes from '../screens/Themes'
 
-export default StackNavigator({
+const HomeStack = StackNavigator({
   Home: {
     screen: Home,
     navigationOptions: {
@@ -12,15 +14,42 @@ export default StackNavigator({
       header: () => null,
     },
   },
+  Options: {
+    screen: Options,
+    navigationOptions: {
+      headerTitle: 'Options',
+    },
+  },
+  Themes: {
+    screen: Themes,
+    navigationOptions: {
+      headerTitle: 'Themes',
+    },
+  },
+}, {
+  headerMode: 'screen', // Navigation bar comes and goes *with* screen, rather than being continuosly on screen which is the default on ios.
+})
+
+const currencyListStack = StackNavigator({
   CurrencyList: {
     screen: CurrencyList,
     navigationOptions: ({ navigation }) => ({
       headerTitle: navigation.state.params.title, // This is how to access the title on top as specified in './screens/Home' handlePressBaseCurrency and handlePressQuoteCurrency functions
     }),
   },
+})
+
+export default StackNavigator({
+  Home: {
+    screen: HomeStack,
+  },
+  CurrencyList: {
+    screen: currencyListStack,
+  },
 }, {
   mode: 'modal',
   cardStyle: {
     paddingTop: StatusBar.currentHeight,
   },
+  headerMode: 'none', // This gets rid of double headers on top
 })
