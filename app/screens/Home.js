bdec365b2a77a69c89dc480277562a6c5a7c4161
Jae Huh <jaeeunhuh@gmail.com>
2017-07-31 13:43:54 +1000

    Set up redux actions for currencies - Home Screen: Actions

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -9,6 +9,8 @@ import { ClearButton } from '../components/Buttons'
 import { LastConverted } from '../components/Text'
 import { Header } from '../components/Header'
 
+import { swapCurrency, changeCurrencyAmount } from '../actions/currencies'
+
 const TEMP_BASE_CURRENCY = 'USD'
 const TEMP_QUOTE_CURRENCY = 'GBP'
 const TEMP_BASE_PRICE = '100'
@@ -31,12 +33,14 @@ class Home extends Component {
     this.props.navigation.navigate('CurrencyList', { title: 'Quote Currency' })
   }
 
-  handleTextChange = (text) => {
-    console.log('change text', text)
+  handleTextChange = (amount) => {
+    // TODO: Make this actually work with this.props.dispatch
+    console.log(changeCurrencyAmount(amount))
   }
 
   handleSwapCurrency = () => {
-    console.log('press swap currency')
+    // TODO: Make this actually work with this.props.dispatch
+    console.log(swapCurrency())
   }
 
   handleOptionsPress = () => {
