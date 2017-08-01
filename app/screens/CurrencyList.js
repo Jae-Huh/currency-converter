02e52cce7ddc5e2ec2bdb810e2e59d81cef2d863
Jae Huh <jaeeunhuh@gmail.com>
2017-08-01 14:06:28 +1000

    Access store to mark selected currency in currency list view - Currency List Screen: Reading Currency Data from the Store

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -7,27 +7,30 @@ import { ListItem, Separator } from '../components/List'
 import currencies from '../data/currencies'
 import { changeBaseCurrency, changeQuoteCurrency } from "../actions/currencies"
 
-const TEMP_CURRENT_CURRENCY = 'CAD'
-
 class CurrencyList extends Component {
   static propTypes = {
     navigation: PropTypes.object,
     dispatch: PropTypes.func,
+    baseCurrency: PropTypes.string,
+    quoteCurrency: PropTypes.string,
   }
 
   handlePress = (currency) => {
     const { type } = this.props.navigation.state.params
     if (type === 'base') {
-      // TODO: Dispatch change base
       this.props.dispatch(changeBaseCurrency(currency))
     } else if (type === 'quote') {
-      // TODO: Dispatch change quote
       this.props.dispatch(changeQuoteCurrency(currency))
     }
     this.props.navigation.goBack(null)
   }
 
   render() {
+    let comparisonCurrency = this.props.baseCurrency
+    if (this.props.navigation.state.params.type === 'quote') {
+      comparisonCurrency = this.props.quoteCurrency
+    }
+
     return (
       <View style={{ flex: 1 }}>
         <StatusBar translucent={false} barstyle="default" />
@@ -36,7 +39,7 @@ class CurrencyList extends Component {
           renderItem={({ item }) => (
             <ListItem
               text={item}
-              selected={item === TEMP_CURRENT_CURRENCY}
+              selected={item === comparisonCurrency}
               onPress={() => this.handlePress(item)}
             />
           )}
@@ -48,4 +51,11 @@ class CurrencyList extends Component {
   }
 }
 
-export default connect()(CurrencyList)
+const mapStateToProps = (state) => {
+  return {
+    baseCurrency: state.currencies.baseCurrency,
+    quoteCurrency: state.currencies.quoteCurrency,
+  }
+}
+
+export default connect(mapStateToProps)(CurrencyList)
