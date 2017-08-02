86092f08c6bb3b47a5caba5fd0178c09c989b2c7
Jae Huh <jaeeunhuh@gmail.com>
2017-08-02 15:07:02 +1000

    Get responses from remote api - Updating Store from API Response

--- app/config/sagas.js
+++ app/config/sagas.js
@@ -1,13 +1,30 @@
-import { takeEvery } from 'redux-saga/effects' // allows us to listen to any actions we want to and when that action is emitted through the redux store, take that action and call a function we specify
+import { takeEvery, select, call, put } from 'redux-saga/effects' // allows us to listen to any actions we want to and when that action is emitted through the redux store, take that action and call a function we specify
 
 // 1. Swap currency
 // 2. Change base currency
 // 3. Make a request upon initial app load
 
-import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies'
+import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION, CONVERSION_RESULT, CONVERSION_ERROR } from '../actions/currencies'
+
+const getLatestRate = currency => fetch(`http://api.fixer.io/latest?base=${currency}`)
 
 function* fetchLastestConversionRates(action) {
-  console.log('TODO: update the things', action)
+  try {
+    let currency = action.currency
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency)
+    }
+    const response = yield call(getLatestRate, currency)
+    const result = yield response.json() // yield allows to wait for the request to happen
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error })
+    } else {
+      yield put({ type: CONVERSION_RESULT, result })
+    }
+  } catch (e) {
+    yield put({ type: CONVERSION_ERROR, error: e.message })
+  }
+
 }
 
 // function* means it's a generator function for setting up a middleware
